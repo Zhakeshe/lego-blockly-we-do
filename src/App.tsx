882945570f8661505f95345e6846ef5debb94d6b
Zhakeshe <zhakezh3@gmail.com>
2025-11-11 10:07:12 +0500

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -3,27 +3,40 @@ import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { AppLayout } from "@/components/AppLayout";
 import Index from "./pages/Index";
+import Projects from "./pages/Projects";
 import Visual from "./pages/Visual";
 import Constructor from "./pages/Constructor";
+import ProjectEditor from "./pages/ProjectEditor";
 import NotFound from "./pages/NotFound";
+
+// ЖАҢА: 3D Simulator
+import IntegratedSimulator from "./pages/IntegratedSimulator";
+import { Robot3DProvider } from "./contexts/Robot3DContext";
 
 const queryClient = new QueryClient();
 
 const App = () => (
   <QueryClientProvider client={queryClient}>
-    <TooltipProvider>
-      <Toaster />
-      <Sonner />
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<Index />} />
-          <Route path="/visual" element={<Visual />} />
-          <Route path="/constructor" element={<Constructor />} />
-          <Route path="*" element={<NotFound />} />
-        </Routes>
-      </BrowserRouter>
-    </TooltipProvider>
+    <Robot3DProvider>
+      <TooltipProvider>
+        <Toaster />
+        <Sonner />
+        <BrowserRouter>
+          <AppLayout>
+            <Routes>
+              <Route path="/" element={<Index />} />
+              <Route path="/projects" element={<Projects />} />
+              <Route path="/editor/:projectId" element={<ProjectEditor />} />
+              <Route path="/constructor" element={<Constructor />} />
+              <Route path="/visual" element={<IntegratedSimulator />} />
+              <Route path="*" element={<NotFound />} />
+            </Routes>
+          </AppLayout>
+        </BrowserRouter>
+      </TooltipProvider>
+    </Robot3DProvider>
   </QueryClientProvider>
 );
 

1bbba4474f3fa02553a3dcb94be543088fa7058d
Zhakeshe <zhakezh3@gmail.com>
2025-11-11 10:11:31 +0500

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -8,29 +8,35 @@ import Index from "./pages/Index";
 import Projects from "./pages/Projects";
 import Visual from "./pages/Visual";
 import Constructor from "./pages/Constructor";
-import ProjectEditor from "./pages/ProjectEditor"; // New Editor Page
+import ProjectEditor from "./pages/ProjectEditor";
 import NotFound from "./pages/NotFound";
+
+// ЖАҢА: 3D Simulator
+import IntegratedSimulator from "./pages/IntegratedSimulator";
+import { Robot3DProvider } from "./contexts/Robot3DContext";
 
 const queryClient = new QueryClient();
 
 const App = () => (
   <QueryClientProvider client={queryClient}>
-    <TooltipProvider>
-      <Toaster />
-      <Sonner />
-      <BrowserRouter>
-        <AppLayout>
-          <Routes>
-            <Route path="/" element={<Index />} />
-            <Route path="/projects" element={<Projects />} />
-            <Route path="/editor/:projectId" element={<ProjectEditor />} /> {/* New Editor Route */}
-            <Route path="/visual" element={<Visual />} />
-            <Route path="/constructor" element={<Constructor />} />
-            <Route path="*" element={<NotFound />} />
-          </Routes>
-        </AppLayout>
-      </BrowserRouter>
-    </TooltipProvider>
+    <Robot3DProvider>
+      <TooltipProvider>
+        <Toaster />
+        <Sonner />
+        <BrowserRouter>
+          <AppLayout>
+            <Routes>
+              <Route path="/" element={<Index />} />
+              <Route path="/projects" element={<Projects />} />
+              <Route path="/editor/:projectId" element={<ProjectEditor />} />
+              <Route path="/constructor" element={<Constructor />} />
+              <Route path="/visual" element={<IntegratedSimulator />} />
+              <Route path="*" element={<NotFound />} />
+            </Routes>
+          </AppLayout>
+        </BrowserRouter>
+      </TooltipProvider>
+    </Robot3DProvider>
   </QueryClientProvider>
 );
 
